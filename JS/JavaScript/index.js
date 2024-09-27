8cc69533a1c4ab61eee1cdcbd02c1cf5b8e78cfb
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-09-27 21:19:35 +0600

    simple validation test

---
+++ JS/JavaScript/index.js
@@ -0,0 +1,10 @@
+function validateForm(){
+    var first_name=document.getElementById("first_name").value;
+
+    if(first_name ==""){
+        alert("Please Enter Your First Name")
+        return false;
+    }
+   
+}
+
